e43f0b3d4ca53b6dcc7b7dd13675669cc128a514
Joseph Rojo <jsimon.rojo@gmail.com>
2018-02-18 22:33:45 +0800

    added percentage section

--- src/components/GoalItem.vue
+++ src/components/GoalItem.vue
@@ -1,9 +1,14 @@
 <template>
-  <section class="goal-item">
-    <h2 class="goal-name">{{ item.name }}</h2>
-    <div><span>Spent: {{ item.spent }}</span></div>
-    <div><span>Limit: {{ item.limit }}</span></div>
-  </section>
+  <div class="grid-container">
+    <section class="goal-item">
+      <h2 class="goal-name">{{ item.name }}</h2>
+      <div><span>Spent: {{ item.spent }}</span></div>
+      <div><span>Limit: {{ item.limit }}</span></div>
+    </section>
+    <section class="percentage">
+      Percentage Section
+    </section>
+  </div>
 </template>
 
 <script>
@@ -18,16 +23,28 @@
 </script>
 
 <style scoped lang="scss">
-  .goal-item {
+  .grid-container {
+    background: white;
     margin-bottom: 1px;
     padding: 0 1rem 1rem;
 
-    background: white;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-areas: "detail detail percentage";
+  }
+
+  .goal-item {
+
     text-align: left;
+    grid-area: detail;
   }
 
   .goal-name {
     margin: 0;
     padding: 1rem 0;
+  }
+
+  .percentage {
+    grid-area: percentage;
   }
 </style>
